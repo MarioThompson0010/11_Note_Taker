cedc09dcfcef011101f67bd5bd15fa1ae83f97dd
Mario Thompson <MarioThompson0010@gmail.com>
2020-11-20 11:58:47 -0600

    begin to set up structure of program

---
+++ Solved/public/routes/apiRoutes.js
@@ -0,0 +1,6 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+var router = require('express').Router();
