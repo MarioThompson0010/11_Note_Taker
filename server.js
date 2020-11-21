685e34c0bab27ff12a370e9a1111356b1b5f5334
Mario Thompson <MarioThompson0010@gmail.com>
2020-11-21 15:09:50 -0600

    reorganized file structure so heroku would work

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // ==============================================================================
 
 const express = require('express');
-const routes = require('../routes');
+const routes = require('./routes');
 
 // Sets up the Express App
 // =============================================================
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("../public"));
+app.use(express.static("./public"));
 
 // ================================================================================
 // ROUTER
